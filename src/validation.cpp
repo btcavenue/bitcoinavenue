e8f223e35da0208f485d879e248c3527ee2a14f0
btcavenue <62223458+btcavenue@users.noreply.github.com>
2020-03-16 00:50:31 +0100

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1223,11 +1223,13 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
+    if (nHeight == 5) return COIN * 1000000;
     CAmount nSubsidy = 50 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
 }
+
 
 CoinsViews::CoinsViews(
     std::string ldb_name,
